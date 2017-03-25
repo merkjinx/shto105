a2381439041e7c415469dfcf79a8c997049a05fd
merkjinx <merkjinx@users.noreply.github.com>
2017-03-25 16:02:39 -0700

    Create rtexoand.sh

---
+++ rtexoand.sh
@@ -0,0 +1,28 @@
+echo -n "what is the chain length:"
+read length
+echo -n "what is the password length"
+read passlength
+echo "starting..."
+
+filelen=$(wc -l rt.txt | cut -c 6-9)
+
+for (( i = 1; i <= $filelen; i++))
+do
+rthash=$(sed -n "$i"p rt.txt )
+lad1=$(echo $rthash | awk -F '[ \t]+|/' '{print $1}' )
+lad2=$(echo $rthash | awk -F '[ \t]+|/' '{print $2}' )
+start=$(echo $lad1 | md5)
+proc=$start
+ for (( c = 0; c <= $length; c++))
+ do
+  proc=$(echo $parse | md5)
+  parse=$(echo $proc | cut -c 1-$passlength)
+  echo $parse >> gddd.txt
+
+  if [ "$proc" == "$lad2" ]; then
+   echo "Completed chain: $i"
+   break;
+  fi
+ done
+
+done
